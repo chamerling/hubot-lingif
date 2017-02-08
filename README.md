ba4708ecc494ca61baa3a1a2e0de244b412edd40
Christophe Hamerling <chamerling@linagora.com>
2017-02-09 00:17:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# hubot-lingif
+
+A [Hubot](http://hubot.github.com) script listening to lingif commands and sends back a Linagorian GIF.
+
+## Install
+
+- Install via npm
+
+```
+npm install hubot-lingif --save
+```
+- Add the following code in your external-scripts.json file.
+
+```
+["hubot-lingif"]
+```
+
+- Run hubot
+
+```
+bin/hubot
+```
+
+## Usage
+
+```
+hubot> hubot lingif kader
+hubot> http://i.giphy.com/QHKMjJN1sG2sw.gif
+```
+
+![Kader](http://i.giphy.com/QHKMjJN1sG2sw.gif "Kader")
+
+## Add you GIF
+
+Update the db.json file by adding your GIF(s) and send a PR. The format is like:
+
+```
+"firstname,lastname,fullname,login,trigram,whatever": ["http://your.gif"]
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2017 Christophe Hamerling <chamerling@linagora.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,84 @@
+const _ = require('lodash');
+const fs = require('fs');
+const DB_URL = 'https://raw.githubusercontent.com/chamerling/hubot-lingif/master/db.json';
+
+module.exports = (robot) => {
+
+  robot.respond(/lingif (.*)$/i, getGif);
+
+  function getGif(msg) {
+    get(msg.match[1])
+      .then(gif => {
+        robot.logger.debug(`gif result to ${msg.match[1]} is ${gif}`);
+        msg.send(gif);
+      })
+      .catch(err => robot.logger.error(err));
+  }
+
+  function get(text) {
+    return load().then(data => {
+      if (!data[text] || !data[text].length) {
+        throw new Error(`Can not find matching result for ${text}`);
+      }
+
+      return _.sample(data[text]);
+    });
+  }
+
+  function load() {
+    return loadRemoteDatabase()
+      .catch(err => {
+        robot.logger.error(err);
+
+        return loadLocalDatabase();
+      });
+  }
+
+  function loadLocalDatabase() {
+    const promise = new Promise((resolve, reject) => {
+      fs.readFile('./db.json', 'utf8', (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve(JSON.parse(data));
+      });
+    });
+
+    return promise;
+  }
+
+  function loadRemoteDatabase() {
+    const promise = new Promise((resolve, reject) => {
+      robot
+        .http(DB_URL)
+        .header('Accept', 'application/json')
+        .get()((err, res, body) => {
+          if (body) {
+            const db = parseDB(JSON.parse(body));
+
+            return resolve(db);
+          }
+
+          reject(err);
+        });
+    });
+
+    return promise;
+  }
+
+  function parseDB(json) {
+    const result = {};
+    _.forEach(json, (value, key) => {
+      _.map(key.split(','), _.trim).forEach(key => {
+        if (!result[key]) {
+          result[key] = [];
+        }
+
+        result[key] = _.concat(result[key], value);
+      });
+    });
+
+    return result;
+  }
+};
